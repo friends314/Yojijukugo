736b90bab43215c0bf8a1a60e26d93950d15ae70
friends314 <demizu@friends314.com>
2023-05-21 03:08:01 +0900

    cannot read property エラーの修正

--- js/main.js
+++ js/main.js
@@ -1,9 +1,13 @@
 'use strict';
 
-document.querySelector("#submit").addEventListener('click', () => {
-    const answer = document.querySelector("#answer").value;
-    // console.log(answer);
-    if(answer === 'ぶんしつひんぴん'){
-        alert('正解！');
-    }
-})
+const submit = document.querySelector("#submit");
+
+if (submit) {
+    submit.addEventListener('click', () => {
+        const answer = document.querySelector("#answer").value;
+        // console.log(answer);
+        if (answer === 'ぶんしつひんぴん') {
+            alert('正解！');
+        }
+    })
+}
