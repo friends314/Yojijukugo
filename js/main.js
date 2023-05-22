fd23b08f190346ad933447fa19f9bbd394250a24
Satoshi Demizu <demizu@friends314.com>
2023-05-22 23:29:46 +0900

    辞書データを追加、ランダムで表示させる

--- js/main.js
+++ js/main.js
@@ -2,11 +2,23 @@
 
 const submit = document.querySelector("#submit");
 
+/**
+ * 辞書から１つランダムに取得
+ * @meth Math.floor(Math.random() * (max - min) + min);
+ */
+const key = Math.floor(Math.random() * (dictionary.length));
+const obj = dictionary[key];
+
+// HTML初期表示
+document.querySelector("#jukugo").textContent = obj.jukugo;
+document.querySelector("#meaning").textContent = obj.meaning;
+
+
 if (submit) {
     submit.addEventListener('click', () => {
         const answer = document.querySelector("#answer").value;
         // console.log(answer);
-        if (answer === 'ぶんしつひんぴん') {
+        if (answer === obj.kana) {
             alert('正解！');
         }
     })
