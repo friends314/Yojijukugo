a7205993250cb89d6cd06180824210fd142ec681
friends314 <demizu@friends314.com>
2023-05-24 04:00:29 +0900

    ・確定ボタンを廃止し、Enterキーで答えを判定する仕様に変更 ・次の問題に遷移したら入力フォームを空にする ・問題が表示された際、入力フォームにフォーカスさせる ・入力フォームの枠線の色付け、正解は水色、間違いは赤

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 'use strict';
 
-const submit = document.querySelector("#submit");
+const jukugo = document.querySelector("#jukugo");
+const answer = document.querySelector("#answer");
 const reveal = document.querySelector("#reveal");
 
 /**
@@ -10,8 +11,11 @@ const reveal = document.querySelector("#reveal");
  */
 const tmp = []; // 除外用配列
 const getQuestion = (d = dictionary) => {
+    // 入力フォームを初期化
+    answer.value = '';
+
     if (tmp.length == d.length) {
-        alert("お疲れ様でした");
+        jukugo.textContent = 'お疲れ様でした';
         reveal.remove();  // 完了画面の作成を検討
         return false;
     }
@@ -22,7 +26,7 @@ const getQuestion = (d = dictionary) => {
     } while (tmp.includes(key));
 
     tmp.push(key);
-    console.log(tmp);
+    // console.log(tmp);
     return d[key];
 }
 let question = getQuestion();
@@ -32,29 +36,34 @@ let question = getQuestion();
  */
 const displayQuiz = question => {
     // console.log(question);
-    document.querySelector("#jukugo").textContent = question.jukugo;
+    jukugo.textContent = question.jukugo;
     document.querySelector("#meaning").textContent = question.meaning;
+    answer.focus();
 }
 displayQuiz(question);
 
 /**
  * 正解したら次の問題を表示
  */
-const submitAnswer = () => {
-    submit.addEventListener('click', () => {
-        const answer = document.querySelector("#answer").value;
-
-        if (answer === question.kana) {
-            alert('正解！');
-            question = getQuestion();
-            if (question != false){
-                displayQuiz(question);
-                submitAnswer();
+const checkAnswer = () => {
+    answer.addEventListener('keydown', (e) => {
+        console.log(e.keyCode);
+        if (e.keyCode == 13 ) {
+            if(e.target.value === question.kana) {
+                answer.classList.remove('deny');
+                alert('正解！');
+                question = getQuestion();
+                if (question != false) {
+                    displayQuiz(question);
+                    checkAnswer();
+                }
+            }else{
+                answer.classList.add('deny');
             }
         }
     })
 }
-submitAnswer();
+checkAnswer();
 
 reveal.addEventListener('click', () => {
     alert(question.kana);
