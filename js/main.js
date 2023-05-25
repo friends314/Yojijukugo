e4ebc14ff34534e5d0f298e2e485d0c3d9fbb077
friends314 <demizu@friends314.com>
2023-05-25 22:50:55 +0900

    現在何問目かと、進捗率の表示を追加

--- js/main.js
+++ js/main.js
@@ -3,6 +3,12 @@
 const jukugo = document.querySelector("#jukugo");
 const answer = document.querySelector("#answer");
 const reveal = document.querySelector("#reveal");
+const meaning = document.querySelector("#meaning");
+const loc = document.querySelector("#location");
+
+const total = dictionary.length;  // 問題数
+let current = 1;                 // 現在何問目か
+let rate = 0; // 進捗率
 
 /**
  * 辞書から１つランダムに取得
@@ -14,15 +20,21 @@ const getQuestion = (d = dictionary) => {
     // 入力フォームを初期化
     answer.value = '';
 
-    if (tmp.length == d.length) {
+    if (tmp.length == total) {
         jukugo.textContent = 'お疲れ様でした';
+
+        const current_location = getLocation(current, total);
+        loc.textContent = current_location;
+
         reveal.remove();  // 完了画面の作成を検討
+        meaning.remove();
+        answer.remove();
         return false;
     }
 
     let key;
     do {
-        key = Math.floor(Math.random() * d.length);
+        key = Math.floor(Math.random() * total);
     } while (tmp.includes(key));
 
     tmp.push(key);
@@ -37,8 +49,10 @@ let question = getQuestion();
 const displayQuiz = question => {
     // console.log(question);
     jukugo.textContent = question.jukugo;
-    document.querySelector("#meaning").textContent = question.meaning;
+    meaning.textContent = question.meaning;
     answer.focus();
+    const current_location = getLocation(current, total);
+    loc.textContent = current_location;
 }
 displayQuiz(question);
 
@@ -47,18 +61,21 @@ displayQuiz(question);
  */
 const checkAnswer = () => {
     answer.addEventListener('keydown', (e) => {
-        console.log(e.keyCode);
+        // console.log(e.keyCode);
         if (e.keyCode == 13 ) {
-            if(e.target.value === question.kana) {
-                answer.classList.remove('deny');
+            // console.log(e.target.value);
+            if (e.target.value === question.kana) {
                 alert('正解！');
+                current += 1;
                 question = getQuestion();
                 if (question != false) {
                     displayQuiz(question);
                     checkAnswer();
                 }
-            }else{
+            } else if (e.target.value !== ''){
                 answer.classList.add('deny');
+            }else{
+                answer.classList.remove('deny');
             }
         }
     })
@@ -68,3 +85,17 @@ checkAnswer();
 reveal.addEventListener('click', () => {
     alert(question.kana);
 })
+
+/**
+ * 現在何問目か＋進捗率を取得
+ */
+function getLocation(current, total) {
+    rate = Math.floor((current - 1) / total * 100);
+    // console.log (current);
+    // console.log(total);
+    if(current - 1 == total){
+        return `全問終わり（進捗率${rate}%）`;
+    }else {
+        return `現在【${current}/${total}】問目（進捗率${rate}%）`;
+    }
+}
